d7cd5d270e0ff4b268ae534d84deabec160d32b0
sherlockwisdom <wisdomnji@gmail.com>
2026-04-02 16:07:06 +0100

    update: checking things

--- bridges/conversations.go
+++ bridges/conversations.go
@@ -10,43 +10,9 @@ import (
 	"github.com/shortmesh/core/configs"
 	"github.com/shortmesh/core/devices"
 	"github.com/shortmesh/core/rabbitmq"
-	"github.com/shortmesh/core/utils"
 	"maunium.net/go/mautrix"
 	"maunium.net/go/mautrix/event"
 )
-
-func checkIfLoginMessage(bridgeConfig configs.BridgeConfig, message string) (*string, error) {
-	cmd := bridgeConfig.Cmd["list-logins"]
-	cmd = regexp.QuoteMeta(cmd)
-	regexPattern := strings.ReplaceAll(cmd, "%s", ".*")
-	matched, err := regexp.MatchString(regexPattern, message)
-	if err != nil {
-		slog.Error(err.Error())
-		debug.PrintStack()
-		return nil, err
-	}
-
-	if matched {
-		extractedDeviceId, err := utils.ExtractBracketContent(message)
-		if err != nil {
-			slog.Error(err.Error())
-			debug.PrintStack()
-			return nil, err
-		}
-
-		deviceId, err := configs.FormatUsername(bridgeConfig.Name, extractedDeviceId)
-		if err != nil {
-			slog.Error(err.Error())
-			debug.PrintStack()
-			return nil, err
-		}
-
-		return deviceId, nil
-
-	}
-
-	return nil, nil
-}
 
 func checkIfSuccess(bridgeConfig configs.BridgeConfig, message string) (*string, error) {
 	regexPattern := strings.ReplaceAll(bridgeConfig.Cmd["success"], "%s", ".*")
@@ -92,43 +58,14 @@ func checkIsFailedLogin(bridgeConfig configs.BridgeConfig, evt *event.Event) (bo
 	return matched, nil
 }
 
-func checkIfStartConversation(evt *event.Event) (bool, error) {
-	expected := "Created chat with"
-	message := evt.Content.AsMessage().Body
-	if !strings.HasPrefix(message, expected) {
-		return false, nil
-	}
-	return true, nil
-}
-
 /*
 - BAD_CREDENTIALS used when device has been disconnected (this can receive an incoming message), this can be used
 when list-devices is ran to delete devices which are deactivated
 */
 func processIncomingBotMessage(client *mautrix.Client, evt *event.Event, bridgeCfg *configs.BridgeConfig) error {
 	message := evt.Content.AsMessage().Body
-	deviceId, err := checkIfLoginMessage(*bridgeCfg, message)
-	if err != nil {
-		slog.Error(err.Error())
-		debug.PrintStack()
-		return err
-	}
 
-	if deviceId != nil {
-		err = (&devices.Devices{
-			Client:     client,
-			DeviceId:   *deviceId,
-			BridgeName: bridgeCfg.Name,
-		}).Save()
-		if err != nil {
-			slog.Error(err.Error())
-			debug.PrintStack()
-			return err
-		}
-		return err
-	}
-
-	deviceId, err = checkIfSuccess(*bridgeCfg, message)
+	deviceId, err := checkIfSuccess(*bridgeCfg, message)
 	if err != nil {
 		slog.Error(err.Error())
 		debug.PrintStack()
